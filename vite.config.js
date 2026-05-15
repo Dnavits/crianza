feaede47bc3d7a6e86226f3ea4f8c62e31035dd2
Dnavits <terrorgm1@gmail.com>
2026-05-15 18:07:56 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Si despliegas en GitHub Pages con un repositorio llamado 'nombre-repo', cámbialo a '/nombre-repo/'
+  base: '/NOMBRE-REPO/', // Cambia NOMBRE-REPO por el nombre de tu repositorio
 });
